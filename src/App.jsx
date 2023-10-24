5cdb63047b224d0b63c45c20e79ab3035f93befe
Ashish <hiashish4x@gmail.com>
2023-10-25 00:53:28 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -48,9 +48,9 @@ function App() {
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
       
-      <div className="bg-[#6d41a6] min-h-screen py-8">
+      <div className="bg-[#561ca1] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Todo App</h1>
                     <div className="mb-4">
                         <TodoForm/>
                     </div>
