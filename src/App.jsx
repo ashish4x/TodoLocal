d2e0fa10143a7ceb06142f66c6cf205351284810
Ashish <hiashish4x@gmail.com>
2023-10-25 00:31:57 +0530

    Created todo form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts'
+import { TodoForm } from './components'
 
 function App() {
 
@@ -27,14 +28,31 @@ function App() {
   }
   
 
+  useEffect(() => {
+    
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+
+
+  }, [])
+
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
+  
+
   return (
-    <TodoProvider value={{todos,addTodos,updateTodos,deleteTodos,toggleComplete}}>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
       
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#6d41a6] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
