2eee298bf11be23eb315f655721257d516a13c67
Ashish <hiashish4x@gmail.com>
2023-11-08 22:00:29 +0530

    click to copy link updated

--- frontend/src/TodoPage.jsx
+++ frontend/src/TodoPage.jsx
@@ -135,7 +135,7 @@ else{
                     <div className='text-center'>
                     <button 
                     className="px-2 py-2 mb-2 mt-5 text-sm font-medium text-center text-white bg-gray-500 rounded-lg hover:bg-gray-700 "
-  onClick={() =>  navigator.clipboard.writeText('https://localtodoapp.vercel.app/'+todoId)}
+  onClick={() =>  navigator.clipboard.writeText('https://group-todo.vercel.app/'+todoId)}
 >
   Copy the Todo Link 📋
 </button>
